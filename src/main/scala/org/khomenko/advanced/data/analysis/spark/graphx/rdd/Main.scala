a9fb2f65181e54d6e7d9396107c1577d10ff9c98
Roman Khomenko <romankhomenko1995@gmail.com>
2021-03-29 11:31:28 +0300

    Graph saved to CSV

--- src/main/scala/org/khomenko/advanced/data/analysis/spark/graphx/rdd/Main.scala
+++ src/main/scala/org/khomenko/advanced/data/analysis/spark/graphx/rdd/Main.scala
@@ -1,15 +1,19 @@
 package org.khomenko.advanced.data.analysis.spark.graphx.rdd
 
 import java.io.{File, PrintWriter}
-
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.graphx.Graph
-
-import Utils.{loadGraph, saveToGraphML}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.graphx.{Edge, Graph}
+import Utils.{loadGraph, processRawTSV, saveGraph, saveToGraphML}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 object Main extends App {
+  val loadFromTSV = args.length match {
+    case 4 => true
+    case _ => false
+  }
+
   if (args.length < 2) {
-    println("Path to tsv files needed")
+    println("No args provided")
     System.exit(-1)
   }
 
@@ -19,14 +23,21 @@ object Main extends App {
     .appName("graphTest")
     .getOrCreate()
 
-  val (verticesRDD, edgesRDD) = loadGraph(args(0), args(1), spark)
-  val graph: Graph[Vertex, Int] = Graph(verticesRDD, edgesRDD)
+  val graph: Graph[Vertex, Int] = if (loadFromTSV) {
+    val (verticesRDD, edgesRDD) = processRawTSV(args(0), args(1), spark)
+    val graph: Graph[Vertex, Int] = Graph(verticesRDD, edgesRDD)
 
-  val pw = new PrintWriter(new File("log.txt" ))
+    saveToGraphML(verticesRDD, edgesRDD, "graph.graphml")
+    saveGraph(verticesRDD, edgesRDD, args(2), args(3), spark)
+
+    graph
+  } else {
+    loadGraph(args(0), args(1), spark)
+  }
+
+  val pw = new PrintWriter(new File("log.txt"))
   pw.println(s"Edges: ${graph.edges.count()}")
   pw.println(s"Vertices: ${graph.vertices.count()}")
-  pw.println(s"Connected components: ${graph.connectedComponents(1000).vertices.map{ case(_,cc) => cc}.distinct.count()}")
+  pw.println(s"Connected components: ${graph.connectedComponents(1000).vertices.map { case (_, cc) => cc }.distinct.count()}")
   pw.close()
-
-  saveToGraphML(edgesRDD, verticesRDD, "graph.graphml")
 }
--- src/main/scala/org/khomenko/advanced/data/analysis/spark/graphx/rdd/Utils.scala
+++ src/main/scala/org/khomenko/advanced/data/analysis/spark/graphx/rdd/Utils.scala
@@ -1,15 +1,16 @@
 package org.khomenko.advanced.data.analysis.spark.graphx.rdd
 
 import java.io.{File, PrintWriter}
-import org.apache.spark.graphx.Edge
+import org.apache.spark.graphx.{Edge, Graph}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.sql.functions.{col, lit, monotonically_increasing_id}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
 
 object Utils {
 
   private def loadDisease2DrugGraph(path: String, spark: SparkSession)
-               : (RDD[(Long, Vertex)], RDD[Edge[Int]], Dataset[Row]) = {
+  : (RDD[(Long, Vertex)], RDD[Edge[Int]], Dataset[Row]) = {
     val df = spark.read.format("csv")
       .option("sep", "\t")
       .option("inferSchema", "true")
@@ -68,15 +69,73 @@ object Utils {
       .map(row => Edge(row.getAs[Long]("left_id"), row.getAs[Long]("right_id"), 2))
   }
 
-  def loadGraph(disease2DrugPath: String, drug2DrugPath: String, spark: SparkSession)
-               : (RDD[(Long, Vertex)], RDD[Edge[Int]]) = {
+  def processRawTSV(disease2DrugPath: String, drug2DrugPath: String, spark: SparkSession)
+  : (RDD[(Long, Vertex)], RDD[Edge[Int]]) = {
     val (firstNodes, firstEdges, drugs) = loadDisease2DrugGraph(disease2DrugPath, spark)
     val secondEdges = loadDrug2DrugGraph(drug2DrugPath, drugs, spark)
 
     (firstNodes, firstEdges.union(secondEdges))
   }
 
-  def saveToGraphML(edges: RDD[Edge[Int]], vertices: RDD[(Long, Vertex)], path: String): Unit = {
+  def saveGraph(vertices: RDD[(Long, Vertex)],
+                edges: RDD[Edge[Int]],
+                verticesPath: String,
+                edgesPath: String,
+                spark: SparkSession): Unit = {
+
+    val vertexSchema = StructType(List("id", "type", "code")
+      .map(fieldName => StructField(fieldName, StringType, nullable = false)))
+    spark.sqlContext.createDataFrame(vertices.map(it => Row(
+        it._1.toString,
+        it._2.vertexType.id.toString,
+        it._2.code)),
+      vertexSchema)
+      .write
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv(verticesPath)
+
+    val edgeSchema = StructType(List("srcId", "dstId", "attr")
+      .map(fieldName => StructField(fieldName, StringType, nullable = false)))
+    spark.sqlContext.createDataFrame(edges.map(edge => Row(
+        edge.srcId.toString,
+        edge.dstId.toString,
+        edge.attr.toString)),
+      edgeSchema)
+      .write
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .csv(edgesPath)
+  }
+
+  def loadGraph(veticesCSV: String, edgesCSV: String, spark: SparkSession): Graph[Vertex, Int] = {
+    val verticesRDD = spark.read.format("csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .load(veticesCSV)
+      .rdd
+      .map(row => (
+        row.getAs[Long]("id"),
+        Vertex(
+          VertexType(row.getAs[Int]("type")),
+          row.getAs[String]("code"))
+      ))
+
+    val edgesRDD = spark.read.format("csv")
+      .option("inferSchema", "true")
+      .option("header", "true")
+      .load(edgesCSV)
+      .rdd
+      .map(row => Edge(
+        row.getAs[Long]("srcId"),
+        row.getAs[Long]("dstId"),
+        row.getAs[Int]("attr")
+      ))
+
+    Graph(verticesRDD, edgesRDD)
+  }
+
+  def saveToGraphML(vertices: RDD[(Long, Vertex)], edges: RDD[Edge[Int]], path: String): Unit = {
     val begin =
       """<?xml version="1.0" encoding="UTF-8"?>
         |<graphml xmlns="http://graphml.graphdrawing.org/xmlns"
